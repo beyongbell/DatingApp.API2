be186f64e28381a811f1357019c63ebfdb30a4d6
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-12-22 23:35:50 +0700

    Validate User

--- DatingApp.API2/Dtos/UserForRegisterDto.cs
+++ DatingApp.API2/Dtos/UserForRegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters")]
         public string Password { get; set; }
     }
 }
