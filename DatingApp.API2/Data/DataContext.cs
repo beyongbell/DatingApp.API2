fb78a2799014d15f33985105e79a7c11aea647dc
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-12-22 16:10:59 +0700

    Add User In Models

--- DatingApp.API2/Data/DataContext.cs
+++ DatingApp.API2/Data/DataContext.cs
@@ -11,5 +11,6 @@ namespace DatingApp.API2.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
