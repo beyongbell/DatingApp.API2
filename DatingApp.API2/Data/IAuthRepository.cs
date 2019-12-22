595392099b160d9c6e97e1eb20f573062f2f5003
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-12-22 20:54:22 +0700

    Data AuthRepository

---
+++ DatingApp.API2/Data/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using DatingApp.API2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API2.Data
+{
+    interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
--- DatingApp.API2/Startup.cs
+++ DatingApp.API2/Startup.cs
@@ -30,6 +30,7 @@ namespace DatingApp.API2
             services.AddDbContext<DataContext>(config => config.UseSqlite((Configuration.GetConnectionString("DefaultConnection"))));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
