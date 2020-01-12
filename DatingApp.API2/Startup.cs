5a4a5ce28c53b3196652e3ba5f388b369a328f59
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-01-12 16:50:08 +0700

    Cloudinary Added

--- DatingApp.API2/Startup.cs
+++ DatingApp.API2/Startup.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Helpers;
 using DatingApp.API2.Data;
+using DatingApp.API2.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
@@ -41,6 +42,7 @@ namespace DatingApp.API2
                     opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
