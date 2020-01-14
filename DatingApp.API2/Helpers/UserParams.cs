cc4eae3e703b6495deeea8ef3f655f5fbecb203f
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-01-14 08:41:45 +0700

    Add Filter Gender And Min Max Age

--- DatingApp.API2/Helpers/UserParams.cs
+++ DatingApp.API2/Helpers/UserParams.cs
@@ -15,5 +15,9 @@ namespace DatingApp.API2.Helpers
             get { return pageSize; }
             set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
         }
+        public int UserId { get; set; }
+        public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 99;
     }
 }
